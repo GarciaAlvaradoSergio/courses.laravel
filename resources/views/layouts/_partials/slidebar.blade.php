e7bf325000194527c2d9d6ee5bf616793910121a
Sergio Garcia <al222110820@gmail.com>
2024-03-20 21:17:48 -0600

    Boton de Profile

--- resources/views/layouts/_partials/slidebar.blade.php
+++ resources/views/layouts/_partials/slidebar.blade.php
@@ -29,7 +29,7 @@
                     </a>
                     <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
                         <li><a class="dropdown-item" href="#">Mis cursos</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Profile</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,9 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body class="font-sans antialiased">
