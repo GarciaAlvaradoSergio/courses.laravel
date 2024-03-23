bee6758b5e52783b68bfb25172831bcaa02b3e70
Sergio Garcia <al222110820@gmail.com>
2024-03-23 14:22:20 -0600

    View: Welcome --corrección en Links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
 
     <nav class="navbar navbar-expand-lg bg-success" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">Tutor UTVT</a>
+            <a class="navbar-brand" href="{{ route('dashboard') }}">Tutor UTVT</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
